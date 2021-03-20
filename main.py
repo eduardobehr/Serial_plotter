62556a5f344d0ed3f25b7491dfee84b45e7a7640
Eduardo <eduardo.behr@hotmail.com>
2021-03-20 15:57:23 -0300

    Fixed: 1) Multiple variables can now be plotted simultaneously (each variable has its own buffer index to keep track of)
    2) Variables created from corrupted strings received from serial are now removed from plot if not updated for a given time.
    
    Todo: add legend, improve efficiency, increase capabilities

--- main.py
+++ main.py
@@ -16,14 +16,23 @@ from serial import EIGHTBITS, SEVENBITS, PARITY_NONE, PARITY_ODD, PARITY_EVEN, S
     SerialException, SerialTimeoutException
 from serial.tools.list_ports import main as list_ports
 from serial.tools.list_ports import comports
+from ctypes import c_float
 
 # FIXME: When more signals are sent, the plotter interleaves them switching to zero (like a saw tooth wave)
+#   It happens because the buffer index is global and increments with the update of each var, leaving the other at zero
 # FIXME: sometimes, the plot freezes on start, although the data is being received
+#   The above happens when the serial is caught in the middle of a transaction, and the string gets currupted!
+#   Check if the value is correct?
+#   Alternative: if the variable is not updated after N sweeps, remove it from the plot!
+
+# WARNING: Qt's "emit" rejects type 'float'. Therefore, values must be 'int'
+
 # TODO: JOIN Threads! Secondary thread keeps running despite KeyboardInterrupt
 # GUI Thread should wait for ALL the variables to be updated
 # Constants configuration:
 DEBUG = True
-DATA_BUFFER_LENGTH = 100
+DEBUG_NOISE = True
+DATA_BUFFER_LENGTH = 1100
 PRINT_PARSED_DATA = False
 UPDATE_PERIOD = 10  # milliseconds
 VARIABLES_LIMIT = 1
@@ -32,6 +41,58 @@ ANTIALIASING = True
 # Port selection prompt
 available_ports = [p[0] for p in comports()]
 N_PORTS = len(available_ports)
+
+class Variable:
+    """ Object stores an array with all the values to be plotted
+     Creation: 20/03/2021
+    """
+    instances = dict()
+    n_instances = 0
+    colors = "yrgb"  # possible colors to plot
+    color = colors[len(instances)]
+
+    # def create_var(self, name, init_value):
+    #     if name not in Variable.instances:
+    #         Variable(name, init_value)
+    #     else:
+    #         if DEBUG:
+    #             print()
+
+    def __init__(self, name, init_value):
+        """ WARNING: do not call this method! """
+        self.name = name
+        self.last_value = init_value
+        self.instances.update({self.name: self})
+        self.index = 0  # integer index of the buffer
+        self.id = Variable.n_instances
+        self.updated = False
+        self.buffer = np.zeros(DATA_BUFFER_LENGTH, float)
+        self.new_value(self.last_value)
+        self.color = Variable.colors[self.id%len(Variable.instances)]
+        self.curve = plot_window.plot(pen=self.color)
+        self.last_time_updated = 0
+
+        Variable.n_instances += 1
+
+    def __repr__(self):
+        return f"{self.name} -> (color={self.color}, last_var={self.last_value})"
+
+    def _increment_index(self):
+        self.index = (self.index + 1) % DATA_BUFFER_LENGTH
+        self.updated = False
+
+    def new_value(self, value):
+        self.last_value = value
+        self.buffer[self.index] = self.last_value
+        self.updated = True
+        self.last_time_updated = time.time()
+        self._increment_index()
+
+    def up_to_date(self, time_limit=5):
+        """ Tests if the variable was recently updated """
+        return time.time() - self.last_time_updated < time_limit
+
+
 
 def display_ports():
     for i, port in enumerate(available_ports):
@@ -113,36 +174,48 @@ def data_update_slot(name, value):
     # print(f"Signal received!{t}")
     global variables, updated_variables
 
-    if value >= 0:  # ADC reads no negative value. TODO: allow any real value
+    # if value >= 0:  # ADC reads no negative value. TODO: allow any real value
+    if True:
+
+        #### BEGIN BRANCH 20/03/2021 #############
+        if name not in Variable.instances:
+            Variable(name=name, init_value=value)
+        else:
+            Variable.instances[name].new_value(value)
+
+        if DEBUG: print(Variable.instances)
+
+        #### END BRANCH 20/03/2021 #############
+
         # checks if there is a new variable from serial
-        if name not in variables and len(variables) <= VARIABLES_LIMIT:
-            colors = "yrgb"
-            color = colors[len(variables)]
-
-            updated_variables.append(False)
-            # creates a new ndarray for the new variable
-            variables.update(
-                {
-                    name:
-                        [
-                            np.zeros(DATA_BUFFER_LENGTH, float),
-                            plot_window.plot(pen=color),
-                            False  # updated value state
-                        ]
-
-
-                }
-            )
-        if name in variables:
-            variables[name][0][iter] = value  
-            variables[name][2] = True
+        # if name not in variables and len(variables) <= VARIABLES_LIMIT:
+        #     colors = "yrgb"
+        #     color = colors[len(variables)]
+        #
+        #     updated_variables.append(False)
+        #     # creates a new ndarray for the new variable
+        #     variables.update(
+        #         {
+        #             name:
+        #                 [
+        #                     np.zeros(DATA_BUFFER_LENGTH, float),
+        #                     plot_window.plot(pen=color),
+        #                     False  # updated value state
+        #                 ]
+        #
+        #
+        #         }
+        #     )
+        # if name in variables:
+        #     variables[name][0][iter] = value
+        #     variables[name][2] = True
+            # if DEBUG: print(f"Value assigned! {value} passed to variable {name}")
         # y[iter] = value
         # x[iter] = time.time()-t0
     else:
         # y[iter] = 0
         pass
-    if DEBUG:
-        print(name, value)
+    # if DEBUG: print(name, value)
 
     pass
 
@@ -155,39 +228,17 @@ def update(name, value):
 
     data_update_slot(name, value)  # Updates the global arrays x and the ones in variables
 
+    #### BEGIN BRANCH 20/03/2021  ############
+    for var in Variable.instances.values(): # gets the objects of Variable
+        if var.up_to_date(time_limit=2):
+            var.curve.setData(x, var.buffer)
+        else:
+            # if the variable wasn't updated in the last couple of seconds, clear it out of the plot
+            if var.curve:
+                var.curve.clear()
+        ...
+    #### END BRANCH 20/03/2021  ############
 
-
-    tnow = time.time()
-    # x[iter] = iter
-    # y[iter] = data_update_slot()
-    # curve.setData( data[iter%variations_example])
-    # print("Variables:", variables)
-    for i, (key, value) in enumerate(variables.items()):
-        # value == [ndarray, plotitem]
-        if name in variables and variables[name][2] == True:  # FIXME 20/03/2021: KeyError. Check if key exists!
-
-            dat = value[0]
-
-            curv = value[1]
-            curv.setData(x, dat)
-            variables[name][2] = False
-        # curve2.setData(x,y2)
-        # print("DATA: ", dat)
-        # print("Curv ", curv)
-        pass
-    # curve.setData(x, y)
-
-    # if DEBUG or True:
-    # dt = tnow-told
-    # print(f"Sampling rate: {1/dt:.3f} Hz")
-    # told = tnow
-
-    iter = (iter + 1) % DATA_BUFFER_LENGTH
-    # iter += 1
-    # if iter >= DATA_BUFFER_LENGTH:
-    #     iter = 0
-    #     # x = np.linspace(0, DATA_BUFFER_LENGTH * dt, DATA_BUFFER_LENGTH)
-    #     pass
 
 # TODO: instead of timer, use event driven approach. I.e, update only when new data reachs the data_update_slot!
 timer = QtCore.QTimer()
@@ -228,7 +279,8 @@ class SerialParser(QtCore.QThread):
             # time.sleep(sleep_time)
 
     def send_to_main_thread(self, _name, _value):
-        self.signal.emit(_name, _value)
+
+        self.signal.emit(_name, int(_value)) # FIXME: emit requires type 'int', so perhaps float can't be reliably passed
         # TODO: release lock and allow main thread to proceed
         return
 
@@ -257,15 +309,15 @@ class SerialParser(QtCore.QThread):
 
         if len(line) > 1:
             var_name = line[0]
-            line[1] = re.sub("[^0-9.]", "", line[1])  # removes everything that is not numeric
-            if DEBUG:
-                print("raw value is: ", line[1], '+ noise')
-            
-            
+            line[1] = re.sub("[^0-9.-]", "", line[1])  # removes everything that is not numeric
+            #if DEBUG:
+                #print("raw value is: ", line[1], '+ noise')
+
             try:
                 var_value = int(line[1])
-                if DEBUG:
-                    var_value += np.random.choice([0,10,20,30])
+                if DEBUG_NOISE and DEBUG:
+                    # var_value += np.random.choice([0,10,20,30])
+                    var_value += 10*np.random.randn()
             except ValueError as err:
                 print(err.args[0])
 
@@ -274,7 +326,8 @@ class SerialParser(QtCore.QThread):
 
 # TODO: add automatic port identification (i.e. serial keeps changing from 0 through 2)
 parser = SerialParser()
-parser.start()
+# parser.start()
+
 
 
 
@@ -283,5 +336,6 @@ parser.start()
 if __name__ == '__main__':
     import sys
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        parser.start()
         QtGui.QApplication.instance().exec_()
         # QtGui.QGuiApplication.thread().wait()
